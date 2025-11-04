a68f5d788b1d988a9426e4ec227d00742b82fd7b
Maykel Blanco <Maykelyork@gmail.com>
2025-11-04 09:27:02 -0400

    Actualizacion estetica, forms, create, edit

---
+++ app/Views/template/pieDePagina.php
@@ -0,0 +1,12 @@
+
+
+<footer class="site-footer">
+    <p>
+        &copy; 2025 Derechos Reservados.
+        <br>
+        Diseñado por <strong>Maykel Blanco</strong> y <strong>Jesús Molina</strong>.
+    </p>
+</footer>
+</div>
+    </body>
+</html>
--- app/Views/vistas/crear.php
+++ app/Views/vistas/crear.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>CREAR REGISTRO</h1>
-</body>
-</html>
+<?=$cabecera?> 
+
+
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Datos del Campeón</h5>
+        <p class="card-text">
+            
+            <form method="post" action="" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="my-input">Nombre del Campeon</label>
+                    <input id="Nombre del Campeon" class="form-control" type="text" name="Nombre del Campeon">
+                </div>
+             
+                <div class="form-group">
+                    <label for="Imagen">Imagen del Campeon</label>
+                    <input id="Imagen" class="form-control-file" type="file" name="Imagen">
+                </div>
+            
+                <button class="btn btn-success" type="button">Guardar</button>
+            
+            
+            </form>
+        
+        </p>
+    </div>
+</div>
+
+
+
+<?=$pieDePagina?>
